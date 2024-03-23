2d38a949b1e3dcf0e0d8b175de0dc5c9a82c7621
vijaykumar-01 <163980312+vijaykumar-01@users.noreply.github.com>
2024-03-23 15:41:32 +0530

    Update server.js
    updated port

--- 3tier-web-application/node/server.js
+++ 3tier-web-application/node/server.js
@@ -1,6 +1,6 @@
 var http = require('http')
 
-var port = 4000
+var port = 3000
 
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'})
